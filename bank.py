6af6b73b1de36e1f5ee651bede39b9b5058cae0e
Daniel Park <dapa9788@colorado.edu>
2026-08-03 23:57:19 -0600

    functional sounds and selecting of sounds

--- bank.py
+++ bank.py
@@ -2,8 +2,14 @@ import json
 
 class SoundBank:
     def __init__(self, filename):
+        self.filename = filename
         with open(filename, "r") as file:
             self.data = json.load(file) #self.data becomes a dictionary that carries the data from the file
             
     def get_sound(self, pad):
         return self.data["pads"][str(pad)]
+
+    def set_sound(self, pad, filename):
+        self.data["pads"][str(pad)] = filename
+        with open(self.filename, "w") as file:
+            json.dump(self.data, file, indent=4)
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import sys
 import pygame #sound audios
-from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout
+import os #filename stuff
+from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout, QFileDialog
+from PySide6.QtCore import Qt
 from bank import SoundBank
 
 class MusicPad(QWidget):
     def __init__(self):
         super().__init__() #initializes QWidget
         self.bank = SoundBank("banks/default.json")
+        self.selected_pad = None
+        self.buttons = {}
 
 
         self.setWindowTitle("Music Pad")
@@ -21,9 +25,11 @@ class MusicPad(QWidget):
                 pad_number = row * 4 + col + 1
                 sound = self.bank.get_sound(pad_number)
                 if sound:
-                    button = QPushButton(sound)
+                    name = os.path.splitext(os.path.basename(sound))[0]
+                    button = QPushButton(name)
                 else:
                     button = QPushButton("[empty]")
+                self.buttons[pad_number] = button
                 button.setFixedSize(90, 90)
                 button.clicked.connect(
                     lambda checked, num=pad_number: self.pad_pressed(num)
@@ -32,15 +38,36 @@ class MusicPad(QWidget):
                 layout.addWidget(button, row, col)
 
         self.setLayout(layout)
+        change_button = QPushButton("Change Pad Sound")
+        change_button.clicked.connect(self.change_sound)
+
+        layout.addWidget(change_button, 4, 0, 1, 4)
 
     def pad_pressed(self, number):
+        self.selected_pad = number
         sound = self.bank.get_sound(number)
         if sound:
             pygame.mixer.Sound(sound).play()
         #print(sound)
 
+    def change_sound(self):
+        file, _ = QFileDialog.getOpenFileName(
+            self,
+            "Choose Sound",
+            "sounds",
+            "Audio Files (*.wav)"
+        )
+
+        if file:
+            self.bank.set_sound(self.selected_pad, file)
+            name = os.path.splitext(os.path.basename(file))[0] #fix the text of the button
+            self.buttons[self.selected_pad].setText(name)
+            #print("Done.")
+
+
 app = QApplication(sys.argv) #QApplication is my entire applicaiton
 pygame.mixer.init()
+pygame.mixer.set_num_channels(64)
 window = MusicPad() #QUI object
 
 window.show() #Open the window
